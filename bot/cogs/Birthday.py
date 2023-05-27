1c1be7aa60d05217db2b22b30a744d1163f93d05
anwari-fikri <anwari.fikri@gmail.com>
2023-05-27 11:40:13 +0800

    scheduled_birthday_reminder works

--- bot/cogs/Birthday.py
+++ bot/cogs/Birthday.py
@@ -1,16 +1,64 @@
 import asyncio
 import requests
 from bs4 import BeautifulSoup
-from datetime import datetime, timedelta
+import datetime
+# from datetime import datetime, timedelta
 import discord
 import pandas as pd
-from discord.ext import commands
+from discord.ext import commands, tasks
 from discord import app_commands
+import json
 
+CHANNEL = "./data/set_channel.json"
+
+utc = datetime.timezone.utc
+time = datetime.time(
+    hour=3, minute=37, tzinfo=utc
+)  # 1am utc = 9am gmt+8 (singapore time)
 
 class Birthday(commands.Cog):
     def __init__(self, client: commands.Bot):
         self.client = client
+        try:
+            with open(CHANNEL, "r") as f:
+                self.set_channel = json.load(f)
+        except FileNotFoundError:
+            self.set_channel = []
+
+        self.scheduled_birthday_reminder.start()
+
+    def cog_unload(self):
+        with open(CHANNEL, "w") as f:
+            json.dump(self.set_channel, f)
+
+    @app_commands.command(
+        name="toggle_birthday_reminder",
+        description="Enable/Disable birthday reminder on this channel",
+    )
+    async def toggle_birthday_reminder(self, interaction: discord.Interaction):
+        if interaction.channel_id not in self.set_channel:
+            self.set_channel.append(interaction.channel_id)
+            await interaction.response.send_message(
+                content=f"Birthday Reminder is now **ENABLED** on #{interaction.channel}"
+            )
+        elif interaction.channel_id in self.set_channel:
+            self.set_channel.remove(interaction.channel_id)
+            await interaction.response.send_message(
+                content=f"Birthday Reminder is now **DISABLED** on #{interaction.channel}"
+            )
+
+    @tasks.loop(time=time)
+    async def scheduled_birthday_reminder(self):
+        if self.set_channel != []:
+            for channel_id in self.set_channel:
+                channel = await self.client.fetch_channel(channel_id)
+                today = datetime.datetime.today() + datetime.timedelta(days=4)
+                today = today.strftime("%B %d")
+                birthdays = await self.scrape_birthday_date(today)
+                if birthdays != None:
+                    for birthday_data in birthdays:
+                        await self.send_embed(channel=channel, birthday_data=birthday_data)
+                        await asyncio.sleep(0.5)
 
     @app_commands.command(
         name="get_today_birthday", description="Learn about Blue Archive Birthday Bot commands"
@@ -18,9 +66,8 @@ class Birthday(commands.Cog):
     async def get_today_birthday(self, interaction: discord.Interaction):
         await interaction.response.defer()
         followup = await interaction.followup.send("Retrieving today's birthday...")
-        today = datetime.today()
+        today = datetime.datetime.today() + datetime.timedelta(days=4)
         today = today.strftime("%B %d")
-        #  + timedelta(days=1)
         birthdays = await self.scrape_birthday_date(today)
         if birthdays != None:
             for birthday_data in birthdays:
@@ -45,7 +92,7 @@ class Birthday(commands.Cog):
         for _, result in results.iterrows():
             name = result["Japanese reading"]
             url = f"https://bluearchive.wiki/wiki/{result['Character']}"
-            image = await self.scrape_image(url)
+            image = await self.scrape_character_image(url)
 
             birthday_data = {
                 "name": name,
@@ -57,7 +104,7 @@ class Birthday(commands.Cog):
 
         return birthdays
     
-    async def scrape_image(self, url: str):
+    async def scrape_character_image(self, url: str):
         response = requests.get(url)
 
         soup = BeautifulSoup(response.content, "html.parser")
