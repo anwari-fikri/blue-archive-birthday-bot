2b37b247dfc7438d65afd716068f65971fdefa33
anwari-fikri <anwari.fikri@gmail.com>
2023-06-24 10:08:45 +0800

    [bug] fix date formatting

--- bot/cogs/Birthday.py
+++ bot/cogs/Birthday.py
@@ -177,7 +177,7 @@ class Birthday(commands.Cog):
 
     async def get_today_formatted(self):
         today = datetime.datetime.today()
-        today = today.strftime("%B%e")
+        today = today.strftime("%B %e")
 
         return today
 
